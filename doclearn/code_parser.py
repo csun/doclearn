787bb92956cd80b55f5b92ebce7b975215f1a1f4
Cameron Sun <cameron.csun@gmail.com>
2016-08-01 10:41:17 -0400

    Added function name extractor

---
+++ doclearn/code_parser.py
@@ -0,0 +1,34 @@
+import ast
+
+
+class _FunctionNameLister(ast.NodeVisitor):
+
+    def __init__(self):
+        super(_FunctionNameLister, self).__init__()
+        self.line_function_names = {}
+
+    def visit_Call(self, node):
+        if node.lineno not in self.line_function_names:
+            self.line_function_names[node.lineno] = []
+
+        if isinstance(node.func, ast.Attribute):
+            function_name = node.func.attr
+        else:
+            function_name = node.func.id
+
+        self.line_function_names[node.lineno].append(function_name)
+        self.generic_visit(node)
+
+
+class CodeParser(object):
+
+    def __init__(self, source_string):
+        self._root_node = ast.parse(source_string)
+
+        self._function_name_lister = _FunctionNameLister()
+        self._function_name_lister.visit(self._root_node)
+
+    def getCalledFunctionNamesForLine(self, line):
+        # NOTE ast starts line indices at 1, while we start at 0.
+        # Increment here to deal with that.
+        return self._function_name_lister.line_function_names[line + 1]
---
+++ doclearn/code_parser_test.py
@@ -0,0 +1,54 @@
+import unittest
+
+from .code_parser import CodeParser
+
+class CodeParserFunctionNamesTest(unittest.TestCase):
+
+    def test_single_line(self):
+        parser = CodeParser('fn()')
+
+        function_names = parser.getCalledFunctionNamesForLine(0)
+        self.assertEquals('fn', function_names[0])
+
+    def test_attribute_function(self):
+        parser = CodeParser('abc.asd.fn()')
+
+        function_names = parser.getCalledFunctionNamesForLine(0)
+        self.assertEquals('fn', function_names[0])
+
+    def test_nested_function(self):
+        parser = CodeParser('fn(fn2())')
+
+        function_names = parser.getCalledFunctionNamesForLine(0)
+        self.assertIn('fn', function_names)
+        self.assertIn('fn2', function_names)
+
+    def test_multiple_lines(self):
+        parser = CodeParser('fn()\nfn2()')
+
+        function_names = parser.getCalledFunctionNamesForLine(0)
+        self.assertEquals('fn', function_names[0])
+
+        function_names = parser.getCalledFunctionNamesForLine(1)
+        self.assertEquals('fn2', function_names[0])
+
+    def test_multiline_function(self):
+        parser = CodeParser('fn(\n'
+                            '        a)\n'
+                            'fn2()')
+
+        function_names = parser.getCalledFunctionNamesForLine(0)
+        self.assertEquals('fn', function_names[0])
+
+        function_names = parser.getCalledFunctionNamesForLine(2)
+        self.assertEquals('fn2', function_names[0])
+
+    def test_nested_multiline_function(self):
+        parser = CodeParser('fn(\n'
+                            '        fn2())')
+
+        function_names = parser.getCalledFunctionNamesForLine(0)
+        self.assertEquals('fn', function_names[0])
+
+        function_names = parser.getCalledFunctionNamesForLine(1)
+        self.assertEquals('fn2', function_names[0])
